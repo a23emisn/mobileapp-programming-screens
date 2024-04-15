8424ca5b709ac965222c2b7865185d2fcc83dbc9
a23emisn <a23emisn@student.his.se>
2024-04-15 15:21:21 +0200

    Added a button to the main activity that launches secondary activity. MainActivity is set as parent activity for secondary activity.

--- app/src/main/java/com/example/screens/MainActivity.java
+++ app/src/main/java/com/example/screens/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.screens;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button toSecondaryActivity = findViewById(R.id.magicButton);
+
+        toSecondaryActivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, SecondaryActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
