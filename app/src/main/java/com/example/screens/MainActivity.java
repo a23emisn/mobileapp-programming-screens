3e1f7b36865f9cd95b2ae68d74ede6d1405a3b89
a23emisn <a23emisn@student.his.se>
2024-04-15 15:47:51 +0200

    Added data to the intent, added a new widget to show data from the first activity.

--- app/src/main/java/com/example/screens/MainActivity.java
+++ app/src/main/java/com/example/screens/MainActivity.java
@@ -13,12 +13,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final String högtid = "Kräftskiva";
+        final String datum = "2024/08/08";
         Button toSecondaryActivity = findViewById(R.id.magicButton);
 
         toSecondaryActivity.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this, SecondaryActivity.class);
+                intent.putExtra("celebration", högtid);
+                intent.putExtra("date", datum);
                 startActivity(intent);
             }
         });
--- app/src/main/java/com/example/screens/SecondaryActivity.java
+++ app/src/main/java/com/example/screens/SecondaryActivity.java
@@ -1,6 +1,7 @@
 package com.example.screens;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -9,7 +10,18 @@ public class SecondaryActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        String celebration;
+        String date;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_secondary);
+        Bundle extras = getIntent().getExtras();
+        celebration = extras.getString("celebration");
+        date = extras.getString("date");
+
+
+        TextView textView = findViewById(R.id.TextView);
+
+        // Update TextView content based on the variable
+        textView.setText("Välkommen på " + celebration + " den " + date + ".");
     }
 }
